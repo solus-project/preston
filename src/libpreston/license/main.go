f190214dd3122af4c461df0b4ff08410f1edafb4
Ikey Doherty <ikey@solus-project.com>
2017-08-20 13:28:17 +0100

    license: Add various transform codepaths for pushing a license
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/libpreston/license/main.go
+++ src/libpreston/license/main.go
@@ -18,16 +18,65 @@ package license
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Accumulator handles all of the potential license files within a repository
 type Accumulator struct {
-	unused int
+	hits       map[string]int
+	transforms map[string]string // Bad name to good name mapping
+	spdx       map[string]int    // SPDX Ids
+	lomap      map[string]string // lower to real SPDX name
 }
 
 // NewAccumulator will return a new Accumulator ready for processing.
 func NewAccumulator() *Accumulator {
-	return &Accumulator{}
+	return &Accumulator{
+		hits:       make(map[string]int),
+		transforms: make(map[string]string),
+		spdx:       make(map[string]int),
+		lomap:      make(map[string]string),
+	}
+}
+
+// pushLicenseFinal will push the license hit into the table without transforms
+func (a *Accumulator) pushLicenseFinal(name string) {
+	a.hits[a.lomap[name]] = 1
+}
+
+// isSPDX determines if the record is valid
+func (a *Accumulator) isSPDX(name string) bool {
+	if _, ok := a.spdx[name]; ok {
+		return true
+	}
+	return false
+}
+
+func (a *Accumulator) pushTransform(name string) bool {
+	if transform, ok := a.transforms[name]; ok {
+		a.pushLicenseFinal(transform)
+		return true
+	}
+	return false
+}
+
+// pushLicense will push the name into the table if possible, and attempt transforms
+// on said name until one is hopefully found.
+func (a *Accumulator) pushLicense(nameOrig string) {
+	name := strings.TrimSpace(nameOrig)
+	names := []string{
+		strings.ToLower(name),
+		strings.Replace(strings.ToLower(name), "_", "-", -1),
+	}
+
+	for _, n := range names {
+		if a.isSPDX(n) {
+			a.pushLicenseFinal(name)
+		}
+		if a.pushTransform(n) {
+			return
+		}
+	}
 }
 
 // ProcessPlainLicense will handle LICENSE/LICENCE/COPYING files and determine
