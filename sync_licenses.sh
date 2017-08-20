7f7bd57e9613161eefe577887fe4073542a48062
Ikey Doherty <ikey@solus-project.com>
2017-08-20 14:05:15 +0100

    Add licenses.spdx input file
    This is loosely inspired by the Clear Linux autospec project, however, this
    method uses a more solid hash and will strip all formatting from the files,
    such as tabs, empty lines and spaces. This will allow us to make simpler hash
    comparisons for files, however we'll still need token matching on files to
    yield more results.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ sync_licenses.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+fail_exit(){
+    echo "$1"
+    exit 1
+}
+
+git clone https://github.com/spdx/license-list.git --depth=1 || fail_exit "Failed to clone"
+
+if [[ -e "licenses.spdx" ]]; then
+    rm -v licenses.spdx
+fi
+
+pushd license-list
+
+rm "Updating the SPDX Licenses.txt" -v
+
+for i in *.txt ; do
+        # Strip all whitespace from it due to many licenses being reflowed
+        # Removes blank lines, all tabs, and all spaces
+        sed -e 's/[\t ]//g;/^$/d' -i $i
+        sum=`sha256sum "${i}"|cut -f 1 -d ' '`
+        nom=`echo "$i" | sed 's@\.txt$@@'`
+        echo -e "${sum}\t${nom}" >> ../licenses.spdx
+done
+popd
+rm -rf license-list
